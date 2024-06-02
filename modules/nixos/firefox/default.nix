b05774ed9ac4bb8384e98287575c794097c9ab64
Sebastian Yonekura <sebastian.yonekura@gmail.com>
2024-06-01 21:10:15 -0400

    fix firefox extensions

--- modules/nixos/firefox/default.nix
+++ modules/nixos/firefox/default.nix
@@ -26,20 +26,11 @@ in {
           };
         in
           listToAttrs [
-            # uBlock Origin
             (extension "ublock-origin" "uBlock0@raymondhill.net")
-            # KeePassXC Browser
-            (extension "keepassxc-browser@keepassxc.org"
-              "fcd31e2a-b84f-4b31-bdae-b6b84795576b")
-            # JetBrains ToolBox Extension
-            (extension "{bf9e77ee-c405-4dd7-9bed-2f55e448d19a}"
-              "81db9cd8-94d4-4501-ba59-814fac867326")
-            # Catppuccin Mocha - Blue
-            (extension "{2adf0361-e6d8-4b74-b3bc-3f450e8ebb69}"
-              "2c641677-e3ca-40df-9edb-33e97648d221")
-            # VeePN
-            (extension "{94ed9bbf-a1e2-4e58-81ae-cd16dad818d8}"
-              "0e7c7c74-411c-4f5d-ae83-c6b1cd119bbb")
+            (extension "keepassxc-browser" "keepassxc-browser@keepassxc.org")
+            (extension "jetbrains-toolbox" "{bf9e77ee-c405-4dd7-9bed-2f55e448d19a}")
+            (extension "catppuccin-mocha-blue" "{88b098c8-19be-421e-8ffa-85ddd1f3f004}")
+            (extension "veepn-free-fast-security-vpn" "{94ed9bbf-a1e2-4e58-81ae-cd16dad818d8}")
           ];
       };
       # TODO fixme
