182d2f7dc593152656639a9e3e506f5b4c33f2e6
Sebastian Yonekura <sebastian.yonekura@gmail.com>
2024-07-21 14:00:02 -0400

    add gnome settings

--- modules/nixos/de/gnome/default.nix
+++ modules/nixos/de/gnome/default.nix
@@ -2,10 +2,15 @@
   lib,
   namespace,
   config,
+  pkgs,
   ...
 }:
 with lib.types; let
   cfg = config.${namespace}.modules.de.gnome;
+  gnomeExtensions = with pkgs.gnomeExtensions; [
+    vitals
+    bing-wallpaper-changer
+  ];
 in {
   options.${namespace}.modules.de.gnome.enable = lib.mkOption {
     type = bool;
@@ -20,7 +25,35 @@ in {
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
 
+    environment.systemPackages = gnomeExtensions;
+
     # Remove GNOME bloatware
-    services.gnome.core-utilities.enable = false;
+    environment.gnome.excludePackages = with pkgs.gnome; [
+      pkgs.gnome-tour
+      geary
+      gnome-maps
+      epiphany
+    ];
+
+    snowfallorg.users.${config.${namespace}.user.name}.home.config = {
+      dconf.settings = {
+        "org/gnome/shell" = {
+          disable-user-extensions = false;
+          enabled-extensions = builtins.map (extension: extension.extensionUuid) gnomeExtensions;
+          favorite-apps = with lib.lists;
+            [
+              "org.gnome.Nautilus.desktop"
+            ]
+            ++ optional config.${namespace}.modules.firefox.enable "firefox.desktop"
+            ++ optional config.${namespace}.modules.utils.enable "obsidian.desktop"
+            ++ optional config.${namespace}.modules.utils.enable "ferdium.desktop"
+            ++ optional config.${namespace}.modules.software.enable "code.desktop"
+            ++ optional config.${namespace}.modules.gaming.enable "steam.desktop";
+        };
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+        };
+      };
+    };
   };
 }
