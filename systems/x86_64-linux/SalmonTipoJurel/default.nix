5dc35fea568e1fbb27315e9ab0a1ca9f3c5ea135
Sebastian Yonekura <sebastian.yonekura@gmail.com>
2025-01-19 11:54:59 -0500

    adds laptop config

---
+++ systems/x86_64-linux/SalmonTipoJurel/default.nix
@@ -0,0 +1,90 @@
+# SalmonTipoJurel -> Dev Laptop
+{...}: {
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.modprobeConfig.enable = true;
+  system.stateVersion = "24.05";
+
+  virtualisation.vmVariantWithDisko = {
+    # following configuration is added only when building VM with build-vm
+    virtualisation = {
+      cores = 3;
+      graphics = true;
+      diskSize = 10000;
+    };
+  };
+
+  sy.modules = {
+    photo.enable = true;
+    software.enable = true;
+    utils.enable = true;
+    gaming.enable = true;
+  };
+
+  # Disko setup
+  disko.devices = {
+    disk = {
+      one = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02";
+            };
+            mdadm = {
+              size = "100%";
+              content = {
+                type = "mdraid";
+                name = "raid0";
+              };
+            };
+          };
+        };
+      };
+
+      two = {
+        type = "disk";
+        device = "/dev/nvme1n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02";
+            };
+            mdadm = {
+              size = "100%";
+              content = {
+                type = "mdraid";
+                name = "raid0";
+              };
+            };
+          };
+        };
+      };
+    };
+
+    mdadm = {
+      raid0 = {
+        type = "mdadm";
+        level = 0;
+        content = {
+          type = "gpt";
+          partitions = {
+            primary = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
