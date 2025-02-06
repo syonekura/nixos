b6d391aa8c5f279ef60f31df24a6aa3fd605d11f
Sebastian Yonekura <sebastian.yonekura@gmail.com>
2025-02-05 19:06:24 -0500

    adds video download helper to firefox

--- modules/nixos/firefox/default.nix
+++ modules/nixos/firefox/default.nix
@@ -2,6 +2,7 @@
   lib,
   namespace,
   config,
+  pkgs,
   ...
 }:
 with lib.types; let
@@ -26,15 +27,26 @@ in {
           };
         in
           listToAttrs [
+            # To add additional extensions, find it on addons.mozilla.org, find
+            # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
+            # Then, download the XPI by filling it in to the install_url template, unzip it,
+            # run `jq .browser_specific_settings.gecko.id manifest.json` or
+            # `jq .applications.gecko.id manifest.json` to get the UUID
+            # The UUID also can be found at about:debugging#/runtime/this-firefox
+            # Good read
+            # https://discourse.nixos.org/t/declare-firefox-extensions-and-settings/36265/16
             (extension "ublock-origin" "uBlock0@raymondhill.net")
             (extension "keepassxc-browser" "keepassxc-browser@keepassxc.org")
             (extension "jetbrains-toolbox" "{bf9e77ee-c405-4dd7-9bed-2f55e448d19a}")
             (extension "catppuccin-mocha-blue" "{88b098c8-19be-421e-8ffa-85ddd1f3f004}")
             (extension "veepn-free-fast-security-vpn" "{94ed9bbf-a1e2-4e58-81ae-cd16dad818d8}")
+            (extension "video-downloadhelper" "{b9db16a4-6edc-47ec-a1f4-b86292ed211d}")
           ];
       };
-      # TODO fixme
-      # nativeMessagingHosts = with pkgs; [pkgs.keepassxc];
     };
+    environment.systemPackages = with pkgs; [
+      # Companion app for VideoDownloadHelper
+      vdhcoapp
+    ];
   };
 }
